ca0d1cbffe855ec4be15025eea36502b382f6743
ericksih <erick.saputra08@gmail.com>
2022-01-15 05:50:19 +0700

    Hash Password & Regist Route

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 
 const User = require('../models/User');
 
@@ -17,12 +18,38 @@ router.post(
       'Please, input valid password with 6 or more characters'
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send('cool');
+
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res.status(400).json({ msg: ' User already exists' });
+      }
+
+      user = new User({
+        name,
+        email,
+        password,
+      });
+
+      const salt = await bcrypt.genSalt(10); // 10 is the number of rounds of hashing to perform
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      res.send('user saved');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
   }
 );
 
