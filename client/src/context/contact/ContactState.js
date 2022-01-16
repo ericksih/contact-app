5082e312ab92e24089590296308dc952d946630b
ericksih <erick.saputra08@gmail.com>
2022-01-16 09:17:28 +0700

    Set & clear current contact

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -38,6 +38,7 @@ const ContactState = (props) => {
         type: 'professional',
       },
     ],
+    current: null,
   };
 
   const [state, dispatch] = useReducer(contactReducer, initialState);
@@ -54,8 +55,14 @@ const ContactState = (props) => {
   };
 
   // Set current contact
+  const setCurrent = (contact) => {
+    dispatch({ type: SET_CURRENT, payload: contact });
+  };
 
   // Clear current contact
+  const clearCurrent = () => {
+    dispatch({ type: CLEAR_CURRENT }); // no need payload cause we don't need to pass anything
+  };
 
   // Update contact
 
@@ -67,8 +74,11 @@ const ContactState = (props) => {
     <ContactContext.Provider
       value={{
         contacts: state.contacts,
+        current: state.current,
         addContact,
         deleteContact,
+        setCurrent,
+        clearCurrent,
       }}
     >
       {props.children}
