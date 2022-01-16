07bb2b82cd7cf79e952360219e447aed7e97dfff
ericksih <erick.saputra08@gmail.com>
2022-01-17 01:47:01 +0700

    registration user
    change msg BE on routes -> users,js (   register method ) if user exists

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -1,15 +1,16 @@
 import React, { useReducer } from 'react';
+import axios from 'axios';
 import AuthContext from './authContext';
 import authReducer from './authReducer';
 
 import {
   REGISTER_SUCCESS,
   REGISTER_FAIL,
-  USER_LOADED,
-  AUTH_ERROR,
-  LOGIN_SUCCESS,
-  LOGIN_FAIL,
-  LOGOUT,
+  // USER_LOADED,
+  // AUTH_ERROR,
+  // LOGIN_SUCCESS,
+  // LOGIN_FAIL,
+  // LOGOUT,
   CLEAR_ERRORS,
 } from '../types';
 
@@ -25,14 +26,40 @@ const AuthState = (props) => {
   const [state, dispatch] = useReducer(authReducer, initialState);
 
   // Load User
+  const loadUser = () => console.log('load user ');
 
   // Register User
+  const register = async (FormData) => {
+    // FormData is the data from the form (name, email, password) in Register.js (Register.js)
+    const config = {
+      headers: {
+        // Headers are used to specify the data type and other metadata about the request or response payload.
+        'Content-Type': 'application/json',
+      },
+    };
+
+    try {
+      const res = await axios.post('/api/users', FormData, config);
+      dispatch({
+        type: REGISTER_SUCCESS,
+        payload: res.data,
+      });
+    } catch (err) {
+      dispatch({
+        type: REGISTER_FAIL, // REGISTER_FAIL is a type in authReducer file (authReducer.js) in auth folder
+        payload: err.response.data.msg, // err.response.data.msg is the error message from the server side (server/routes/users.js) in the register() method
+      });
+    }
+  };
 
   // Login User
+  const login = () => console.log('login user ');
 
   // Logout User
+  const logout = () => console.log('logout user ');
 
   // Clear Errors
+  const clearErrors = () => dispatch({ type: CLEAR_ERRORS });
 
   return (
     <AuthContext.Provider
@@ -42,6 +69,11 @@ const AuthState = (props) => {
         loading: state.loading,
         user: state.user,
         error: state.error,
+        register,
+        loadUser,
+        login,
+        logout,
+        clearErrors,
       }}
     >
       {props.children}
